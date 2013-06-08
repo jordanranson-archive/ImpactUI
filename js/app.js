7fe007ce77f7ec7a16d08e7e53d721d6b43ee55d
Jordan Ranson <jordan@jordanranson.com>
2013-06-08 16:05:52 -0600

    Added anchor positioning

--- js/app.js
+++ js/app.js
@@ -21,24 +21,172 @@
 
 
 
-
 // Impact UI
 function App() {
-    var self = this;
-    self.panel = new Panel();
-    self.components = ko.observableArray();
-    self.properties = ko.observableArray();
-    self.zoom = ko.observable(2);
+    var app = this;
+    app.panel = new Panel();
+    app.components = ko.observableArray();
+    app.properties = ko.observableArray();
+    app.zoom = ko.observable(2);
+    
+    
+    
+    // UI panel
+    function Panel() {
+        var self = this;
+        self.id = 0;
+        self.type = "panel";
+        self.display = {};
+        self.display.selected = ko.observable(false);
+        
+        self.name = ko.observable("UI Panel");
+        self.width = ko.observable(512);
+        self.height = ko.observable(374);
+        self.z = ko.observable(0);
+    }
+
+    // Generic UI element
+    function MComponent() {
+        var self = this;
+        self.id = 0;
+        self.type = "none";
+
+        self.display = {};
+        self.display.selected = ko.observable(false);
+        
+        self.name = ko.observable("Component");
+        self.x = ko.observable(0);
+        self.y = ko.observable(0);
+        self.z = ko.observable(0);
+        self.anchor = ko.observable("center-center");
+        self.originX = ko.computed(function() {
+            var anchor = self.anchor().split("-")[1];
+            return app.drawOffset(anchor, 0, app.panel.width());
+        });
+        self.originY = ko.computed(function() {
+            var anchor = self.anchor().split("-")[0];
+            return app.drawOffset(anchor, 0, app.panel.height(), true);
+        });
+    }
+
+    // Label
+    function MLabel() {
+        var self = this;
+        self.id = 0;
+        self.type = "label";
+        
+        self.display = {};
+        self.display.selected = ko.observable(false);
+        
+        self.name = ko.observable("Label");
+        self.text = ko.observable("Label");
+        self.font = ko.observable("04b03");
+        self.x = ko.observable(0);
+        self.y = ko.observable(0);
+        self.z = ko.observable(0);
+        self.width = ko.observable(32);
+        self.height = ko.observable(12);
+        self.lineHeight = ko.observable(1.2);
+        self.anchor = ko.observable("center-center");
+        self.originX = ko.computed(function() {
+            var anchor = self.anchor().split("-")[1];
+            return app.drawOffset(anchor, 0, app.panel.width());
+        });
+        self.originY = ko.computed(function() {
+            var anchor = self.anchor().split("-")[0];
+            return app.drawOffset(anchor, 0, app.panel.height(), true);
+        });
+    }
+
+    // Button
+    function MButton() {
+        var self = this;
+        self.id = 0;
+        self.type = "button";
+        
+        self.display = {};
+        self.display.selected = ko.observable(false);
+        
+        self.name = ko.observable("Button");
+        self.text = ko.observable("Button");
+        self.x = ko.observable(0);
+        self.y = ko.observable(0);
+        self.z = ko.observable(0);
+        self.width = ko.observable(48);
+        self.height = ko.observable(16);
+        self.anchor = ko.observable("center-center");
+        self.originX = ko.computed(function() {
+            var anchor = self.anchor().split("-")[1];
+            return app.drawOffset(anchor, 0, app.panel.width());
+        });
+        self.originY = ko.computed(function() {
+            var anchor = self.anchor().split("-")[0];
+            return app.drawOffset(anchor, 0, app.panel.height(), true);
+        });
+    }
+
+    // Image
+    function MImage() {
+        var self = this;
+        self.id = 0;
+        self.type = "image";
+        
+        self.display = {};
+        self.display.selected = ko.observable(false);
+        
+        self.name = ko.observable("Image");
+        self.src = ko.observable("");
+        self.x = ko.observable(0);
+        self.y = ko.observable(0);
+        self.z = ko.observable(0);
+        self.width = ko.observable(96);
+        self.height = ko.observable(96);
+        self.anchor = ko.observable("center-center");
+        self.originX = ko.computed(function() {
+            var anchor = self.anchor().split("-")[1];
+            return app.drawOffset(anchor, 0, app.panel.width());
+        });
+        self.originY = ko.computed(function() {
+            var anchor = self.anchor().split("-")[0];
+            return app.drawOffset(anchor, 0, app.panel.height(), true);
+        });
+    }
+    
     
     
     // Initialization
-    self.init = function() {
-        Mousetrap.bind("del",   self.removeComponent);
+    app.init = function() {
+        Mousetrap.bind("del",   app.removeComponent);
+    };
+    
+    
+    // Finds the draw offset for computed observables
+    app.drawOffset = function(anchor, pos, panel, direction) {
+        if(direction) {
+            switch(anchor) {
+                case "top":
+                    return Number(pos);
+                case "center":
+                    return Number(pos) + Number(panel) * 0.5;
+                case "bottom":
+                    return Number(pos) + Number(panel);
+            }
+        } else {
+            console.log(anchor);
+            switch(anchor) {
+                case "left":
+                    return Number(pos);
+                case "center":
+                    return Number(pos) + Number(panel) * 0.5;
+                case "right":
+                    return Number(pos) + Number(panel);
+            }
+        }
     };
     
     
     // Displays a modal dialog
-    self.showModal = function(selector) {
+    app.showModal = function(selector) {
         $(".overlay").fadeIn();
         $(".modal").fadeOut();
         $(selector).fadeIn();
@@ -46,14 +194,14 @@ function App() {
     
     
     // Hides all modal dialogs
-    self.hideModals = function() {
+    app.hideModals = function() {
         $(".overlay").fadeOut();
         $(".modal").fadeOut();
     };
     
     
     // Creates a new component and adds it to the list
-    self.createComponent = function(type) {
+    app.createComponent = function(type) {
         var component;
         
         switch(type) {
@@ -63,48 +211,50 @@ function App() {
             default: component = new MComponent();
         }
         
-        self.addComponent(component);
+        console.log(component);
+        component.panel = app.panel;
+        app.addComponent(component);
     };
     
     
     // Removes a component
-    self.removeComponent = function() {
-        self.components.remove(function(item) {
+    app.removeComponent = function() {
+        app.components.remove(function(item) {
             return item.display.selected() === true;
         });
-        self.properties.removeAll();
+        app.properties.removeAll();
     };
     
     
     // Add a component
-    self.addComponent = function(component) {
-        self.components.push(component);
-        self.bindjQuery();
+    app.addComponent = function(component) {
+        app.components.push(component);
+        app.bindjQuery();
         
-        for(var i = 0; i < self.components().length; i++) {
-            self.components()[i].display.selected(false)
+        for(var i = 0; i < app.components().length; i++) {
+            app.components()[i].display.selected(false)
         }
-        self.panel.display.selected(false);
+        app.panel.display.selected(false);
         component.display.selected(true);
-        self.displayProperties(component);
+        app.displayProperties(component);
     };
     
     
     // jQuery crap
-    self.bindjQuery = function() {
+    app.bindjQuery = function() {
         $(".component:not(.label)").resizable({
-            grid: 4 * self.zoom(),
+            grid: 4 * app.zoom(),
             resize: function() {
                 $(this).css("line-height", $(this).height() + "px");
             }
         });
         $(".component.label").resizable({
-            grid: 4 * self.zoom()
+            grid: 4 * app.zoom()
         });
         $(".component").draggable({ 
             containment: ".editor-content", 
             scroll: false,
-            grid: [ 4 * self.zoom(), 4 * self.zoom() ]
+            grid: [ 4 * app.zoom(), 4 * app.zoom() ]
         });
         $(".components-list").sortable({
             axis: "y",
@@ -125,8 +275,8 @@ function App() {
     
     
     // Displays a models properties in the side bar
-    self.displayProperties = function(component) {
-        var model = component === false ? self.panel : component;
+    app.displayProperties = function(component) {
+        var model = component === false ? app.panel : component;
         var properties = [];
         var row, type, value;
         
@@ -155,18 +305,18 @@ function App() {
             }
         }
         
-        self.properties(properties);
+        app.properties(properties);
         
         // Show selected component
-        self.panel.display.selected(false);
-        for(var i = 0; i < self.components().length; i++) {
-            self.components()[i].display.selected(false);
+        app.panel.display.selected(false);
+        for(var i = 0; i < app.components().length; i++) {
+            app.components()[i].display.selected(false);
         }
         model.display.selected(true);
     };
     
     
-    self.init();
+    app.init();
 }
 
 
